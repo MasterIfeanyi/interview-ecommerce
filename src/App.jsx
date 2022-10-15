57b85789d862f9a248c5c7bf5332440686aedc11
Ifeanyi <chimaifeanyi29@gmail.com>
2022-10-15 11:28:36 +0100

    create register endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import { Routes, Route, Navigate } from "react-router-dom"
 import Layout from './Layout/Layout';
+import RequireAuth from './features/auth/RequireAuth';
 import Content from './components/Content';
 import Convert from './components/Convert';
 import MarketWatch from './components/MarketWatch';
-
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<MarketWatch />} />
-        <Route path="convert" element={<Convert />} />
-        <Route path="stocks" element={<Content />} />
+        {/* public routes */}
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+
+        {/* protected routes */}
+        <Route element={<RequireAuth />}>
+          <Route path="profile" element={<Bank />} />
+          <Route path="history/:id" element={<History />} />
+          <Route path="deposit/:id" element={<Deposit />} />
+          <Route path="withdraw/:id" element={<Withdraw />} />
+        </Route>
+
         {/* catch all */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
